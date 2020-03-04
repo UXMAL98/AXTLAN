ebf4d73cf4bc488c501a6ba7ff53ea5c50500a56
UXMAL98 <61767894+UXMAL98@users.noreply.github.com>
2020-03-04 03:42:53 -0600

    Add file Pythonintroduction
    Create Python File Introduction is a format card

---
+++ Pythonintroduction.py
@@ -0,0 +1,22 @@
+Name="Irineo Isaac"
+Last_name="Flores Sanchez"
+Age="19"
+Number="5557274832"
+ID="193112665"
+Nick_Name="Iri"
+Int='**********************************************'
+print (Int)
+print ('hi', Name)
+print (Int)
+print ('Last Name:',Last_name)
+print (Int)
+print ('Youare Age is:', Age ,'years old')
+print (Int)
+print ('Number', Number)
+print (Int)
+print ('ID:',ID)
+print (Int)
+print ('Nick Name:',Nick_Name)
+print (Int)
+print ('Has Diver license?: True')
+print (Int)
